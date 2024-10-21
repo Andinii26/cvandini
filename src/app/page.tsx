a95180536b6fc89109946c8ef50906f37b880b99
Andinii26 <164166206+Andinii26@users.noreply.github.com>
2024-10-21 08:47:31 +0700

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "./components/hero";
+import RiwayatPendidikan from "./components/riwayatpendidikan";
+import RiwayatPekerjaan from "./components/riwayatpekerjaan";
+import "./andini-style.css";
+
+export default function CVOnline(){
+ return(
+<section>
+ <Hero/>
+ <RiwayatPendidikan/>
+ <RiwayatPekerjaan/>
+</section>
+ )
+}
+ 
